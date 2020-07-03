2e5abde99fec3433dbc614e44dede594161eed2b
harmeetonline <64013047+harmeetonline@users.noreply.github.com>
2020-07-03 19:35:55 +0530

    Update game.js

--- game.js
+++ game.js
@@ -18,6 +18,9 @@ var QuesCount = 0
 var CorrectCount = 0
 var WrongCount = 0
 
+var question
+var solution
+
 function newQues() {
   
 
@@ -27,43 +30,50 @@ function newQues() {
   var num2 = Math.floor(Math.random()*10)+4
   var sign = signArr[Math.floor(Math.random()*4)]
 
-  var question = num1+" "+sign+" "+num2
-  var solution = eval(question)
+  question = num1+" "+sign+" "+num2
+  solution = eval(question)
 
   quesDisplay.innerHTML = question
   console.log(question)
   console.log("Solution is "+solution)
 
-  function checkAns(ans) {
+}
 
+  function checkAns(ans) {
+    
     if (solution == ans) {
       // ctx.fillStyle="green"
       // ctx.fillText("Correct!",canvas.width/2, canvas.height/2)
-      quesResult.innerHTML = "Correct"
+      quesResult.style.color="#92C68A";
+      quesResult.innerHTML = "Correct!";
+      
     }
     else {
-      quesResult.innerHTML = "Wrong"
+      quesResult.innerHTML = "Wrong!"
+      quesResult.style.color = "#CB5555";
       // ctx.fillStyle="red"
       // ctx.fillText("Wrong!",canvas.width/2, canvas.height/2)
     }
 
     setTimeout( function () {
+      console.log('ok')
       newQues()
       inp.focus()
       inp.value=""
     }, 300)
   }
 
-  inp.addEventListener('keypress', function(event){
+  inp.addEventListener('keyup', function(event){
+    
     if(event.keyCode==13) {
       if (inp.value!=="") {
         console.log("Enter on "+inp.value);
+        
         // document.getElementById("inp").disabled=true;
         inp.disabled=true;
+        
         checkAns(inp.value)
       }
       else if (inp.value==""){console.log("Enter on Nil")}
    }
   })
-
-}
